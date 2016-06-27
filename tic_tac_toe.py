4021921e067a2e1e8ac2a2c0644ae5308e72c474
Michael Kasa <michael.p.kasa@gmail.com>
2016-06-26 17:46:50 -0700

    Added question mark and wrote a few more lines

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,15 +2,29 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-class TicTacToe(QWidget):
+class TicTacToeTile(QPushButton):
+    def __init__(self, parent, row, col):
+        super(TicTacToeTile, self).__init__(parent)
+        self.row = row
+        self.col = col
+        self.setIcon(QIcon("Beebee.jpg"))
+        self.setIconSize(QSize(180,180))
+
+class TicTacToeBoard(QWidget):
     def __init__(self):
-        super(TicTacToe, self).__init__()
+        super(TicTacToeBoard, self).__init__()
 
         self.reset_btn = QPushButton("Reset", self)
         self.title_label = QLabel("Tic Tac Toe!", self)
         self.prompt_label = QLabel("Welcome to Tic Tac Toe!", self)
 
         self.tilesLayout = QGridLayout()
+        self.tiles = []
+        for i_row in range(3):
+            for i_col in range(3):
+                new_tile = TicTacToeTile(self, i_row, i_col)
+                self.tiles.append(new_tile)
+                self.tilesLayout.addWidget(new_tile,i_row,i_col)
 
         self.messageAndResetLayout = QHBoxLayout()
         self.messageAndResetLayout.addWidget(self.prompt_label)
@@ -26,7 +40,7 @@ class TicTacToe(QWidget):
 
 # Create a QT application
 app = QApplication(sys.argv)
-tic_tac_toe = TicTacToe()
+tic_tac_toe = TicTacToeBoard()
 tic_tac_toe.show()
 
 # Enter Qt application main loop
