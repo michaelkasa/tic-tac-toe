ec189d64d39ee16ed3ec19fffa996cd35d207029
Michael Kasa <michael.p.kasa@gmail.com>
2016-06-26 21:05:54 -0700

    Working code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -18,13 +18,12 @@ class TicTacToeTile(QPushButton):
         self.reset()
 
     def am_clicked(self, current_choice):
-        if self.choice < 0:
+        if not self.is_chosen:
+            self.is_chosen = True
             self.choice = current_choice
             self.setIcon(QIcon(pics_by_choice[current_choice]))
             winner = self.board.check_for_win()
-            if not winner == False:
-                x = 1
-            else:
+            if winner > -1:
                 self.board.lock_all_tiles()
             return True
         else:
@@ -48,6 +47,7 @@ class TicTacToeBoard(QWidget):
         super(TicTacToeBoard, self).__init__()
 
         self.current_choice = 0
+        self.winner = -1
 
         self.reset_btn = QPushButton("Reset", self)
         self.title_label = QLabel("Tic Tac Toe!", self)
@@ -72,17 +72,26 @@ class TicTacToeBoard(QWidget):
         self.masterLayout.addLayout(self.tilesLayout)
         
         self.setLayout(self.masterLayout)
-
         self.reset_btn.clicked.connect(self.reset)
+
+        self.update_prompt()
 
     def clicked_tile(self):
         if self.sender().am_clicked(self.current_choice):
             self.current_choice = (self.current_choice + 1) % 2
+            if self.winner < 0:
+                self.update_prompt()
 
     def reset(self):
         self.current_choice = 0
+        self.winner = -1
         for tile in self.tiles:
             tile.reset()
+        self.update_prompt()
+
+    def update_prompt(self):
+        self.prompt_label.setText('Your turn, ' + \
+                                  names_by_choice[self.current_choice])
 
     def check_for_win(self):
         picks = [[],[],[]]
@@ -90,28 +99,31 @@ class TicTacToeBoard(QWidget):
             for i_col in range(3):
                 i_tile = 3 * i_row + i_col
                 picks[i_row].append(self.tiles[i_tile].get_choice())
-
-        print(picks)
         
         for w in range(2):
-            if [picks[0][0], picks[0][1], picks[0][2]] == [w]*3:
+            if  [picks[0][0], picks[0][1], picks[0][2]] == [w]*3 or \
+                [picks[1][0], picks[1][1], picks[1][2]] == [w]*3 or \
+                [picks[2][0], picks[2][1], picks[2][2]] == [w]*3 or \
+                [picks[0][0], picks[1][0], picks[2][0]] == [w]*3 or \
+                [picks[0][1], picks[1][1], picks[2][1]] == [w]*3 or \
+                [picks[0][2], picks[1][2], picks[2][2]] == [w]*3 or \
+                [picks[0][0], picks[1][1], picks[2][2]] == [w]*3 or \
+                [picks[0][2], picks[1][1], picks[2][0]] == [w]*3:
+                self.prompt_label.setText(names_by_choice[w] + ' wins!')
+                self.winner = w
                 return w
-            elif [picks[1][0], picks[1][1], picks[1][2]] == [w]*3:
-                return w
-            elif [picks[2][0], picks[2][1], picks[2][2]] == [w]*3:
-                return w
-            elif [picks[0][0], picks[1][0], picks[2][0]] ==[w]*3:
-                return w
-            elif [picks[0][1], picks[1][1], picks[2][1]] == [w]*3:
-                return w
-            elif [picks[0][2], picks[1][2], picks[2][2]] == [w]*3:
-                return w
-            elif [picks[0][0], picks[1][1], picks[2][2]] == [w]*3:
-                return w
-            elif [picks[0][2], picks[1][1], picks[2][0]] == [w]*3:
-                return w
-            else:
-                return False
+
+        ''' Check for a tied game! '''
+        for i_row in range(3):
+            for i_col in range(3):
+                if picks[i_row][i_col] < 0:
+                    print(picks)
+                    return -1  # game continues
+
+        print('Made it to here!!!!')
+        self.prompt_label.setText('Tied game!')
+        self.winner = 2
+        return 2  # tied game!
 
     def lock_all_tiles(self):
         for tile in self.tiles:
